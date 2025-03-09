d1b665a6485bde5c9fb81b2ba4d14b1d096fdab2
Ruka <ruka@ruka.my>
2025-03-09 15:13:47 +0900

    build: edit lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
-export default eslintConfig;
+
+export default compat.config({
+  rules: {
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
+});
